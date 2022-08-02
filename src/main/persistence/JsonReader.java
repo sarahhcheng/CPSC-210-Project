3a98b7922051e729d9e7d58d0891a42fd19e8099
sarahhcheng <sarahcheng40@gmail.com>
2022-08-01 17:32:52 -0700

    implemented the json

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -13,7 +13,7 @@ import model.Recipe;
 import model.RecipeBook;
 import org.json.*;
 
-// Represents a reader that reads workroom from JSON data stored in file
+// Represents a reader that reads the RecipeBook from JSON data stored in file
 // Referenced code from the demo example
 public class JsonReader {
     private String source;
@@ -23,7 +23,7 @@ public class JsonReader {
         this.source = source;
     }
 
-    // EFFECTS: reads workroom from file and returns it;
+    // EFFECTS: reads the RecipeBook from file and returns it;
     // throws IOException if an error occurs reading data from file
     public RecipeBook read() throws IOException {
         String jsonData = readFile(source);
@@ -42,7 +42,7 @@ public class JsonReader {
         return contentBuilder.toString();
     }
 
-    // EFFECTS: parses workroom from JSON object and returns it
+    // EFFECTS: parses the RecipeBook from JSON object and returns it
     private RecipeBook parseRecipeBook(JSONObject jsonObject) {
         String title = jsonObject.getString("Title");
         String author = jsonObject.getString("Author");
@@ -81,6 +81,7 @@ public class JsonReader {
         }
     }
 
+    // EFFECTS: grabs the steps of the instructions into a list of instructions
     private ArrayList<String> getRecipeInstructions(JSONObject recipe) {
         JSONArray instructions = recipe.getJSONArray("Instructions");
         ArrayList<String> instructionArray = new ArrayList<>();
@@ -90,6 +91,7 @@ public class JsonReader {
         return instructionArray;
     }
 
+    // EFFECTS: grabs the ingredients needed for a recipe into a list
     private ArrayList<Ingredient> getIngredients(JSONObject recipe) {
         JSONArray instructions = recipe.getJSONArray("Ingredients");
         ArrayList<Ingredient> ingredientsArray = new ArrayList<>();
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -25,7 +25,7 @@ public class JsonWriter {
     }
 
     // MODIFIES: this
-    // EFFECTS: writes JSON representation of workroom to file
+    // EFFECTS: writes JSON representation of the RecipeBook to file
     public void write(RecipeBook rb) {
         JSONObject json = rb.toJson();
         saveToFile(json.toString(TAB));
