53657b3a48d4dc33ae400d679dcb5675cf364f7d
sarahhcheng <sarahcheng40@gmail.com>
2022-08-06 15:56:33 -0700

    pain

--- src/main/ui/RecipeBookFrame.java
+++ src/main/ui/RecipeBookFrame.java
@@ -2,12 +2,23 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class RecipeBookFrame extends JFrame {
     private JPanel banner;
     private JPanel panel;
     private JPanel padding;
     private JPanel bottomPadding;
+
+    private JTextField recipeName;
+    private JTextField ingredients;
+    private JTextField instructions;
+    private ArrayList<JTextField> listofInputs;
+
+
+    private JButton addRecipe;
+    private JButton removeRecipe;
+    private JButton loadRecipe;
 
 
     public RecipeBookFrame() {
@@ -17,6 +28,7 @@ public class RecipeBookFrame extends JFrame {
         getBottomPadding();
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("List of Recipes");
         this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
         this.setSize(1000, 1000);
         this.setLayout(new BorderLayout());
@@ -35,7 +47,7 @@ public class RecipeBookFrame extends JFrame {
         welcome.setFont(new Font("Serif", Font.PLAIN, 30));
         banner.add(welcome);
         banner.setAlignmentX(Component.TOP_ALIGNMENT);
-        banner.setBackground(Color.white);
+        banner.setBackground(new Color(0xE1FCFA));
         banner.setPreferredSize(new Dimension(1000, 75));
         banner.setMaximumSize(new Dimension(1000, 100));
     }
@@ -63,5 +75,38 @@ public class RecipeBookFrame extends JFrame {
         bottomPadding.setPreferredSize(new Dimension(150, 200));
         bottomPadding.setMaximumSize(new Dimension(150, 200));
         bottomPadding.setBackground(new Color(0xE1FCFA));
+        bottomPadding.setLayout(new BoxLayout(bottomPadding, BoxLayout.LINE_AXIS));
+        addRecipe = new JButton("Add");
+        removeRecipe = new JButton("Remove");
+        loadRecipe = new JButton("Load recipe");
+
+        recipeInputFields();
+
+        bottomPadding.add(addRecipe);
+        bottomPadding.add(removeRecipe);
+        bottomPadding.add(loadRecipe);
+        bottomPadding.add(Box.createHorizontalStrut(10));
+        bottomPadding.add(new JSeparator(SwingConstants.VERTICAL));
+        bottomPadding.add(Box.createHorizontalStrut(10));
     }
+
+    public void recipeInputFields() {
+        listofInputs = new ArrayList<>();
+        recipeName = new JTextField();
+        ingredients = new JTextField();
+        instructions = new JTextField();
+
+        listofInputs.add(recipeName);
+        listofInputs.add(ingredients);
+        listofInputs.add(instructions);
+
+        bottomPadding.add(new JLabel("Recipe: "));
+        bottomPadding.add(recipeName);
+        bottomPadding.add(new JLabel("Ingredients: "));
+        bottomPadding.add(ingredients);
+        bottomPadding.add(new JLabel("Instructions: "));
+        bottomPadding.add(instructions);
+    }
+
+
 }
